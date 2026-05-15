3c7e47cde28e4917b2df2176bb8e55f5fe93d7e5
cjm2681 <cjm2681@naver.com>
2026-05-15 20:35:37 +0900

    AllArgsConstructor추가(테스트코드때문)

--- src/main/java/com/secondHand/SecondHandMarket/domain/auth/dto/LoginRequest.java
+++ src/main/java/com/secondHand/SecondHandMarket/domain/auth/dto/LoginRequest.java
@@ -2,11 +2,13 @@ package com.secondHand.SecondHandMarket.domain.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
 
     @NotBlank(message = "이메일을 입력해주세요")
--- src/main/java/com/secondHand/SecondHandMarket/domain/order/dto/TossPaymentRequest.java
+++ src/main/java/com/secondHand/SecondHandMarket/domain/order/dto/TossPaymentRequest.java
@@ -2,11 +2,13 @@ package com.secondHand.SecondHandMarket.domain.order.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class TossPaymentRequest {
 
     @NotBlank
